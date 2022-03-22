ddcfe7545499d7f71d7eb987dfa1482e9eee379f
Elaine5 <lanne9873@gmail.com>
2022-03-22 16:37:51 -0300

    atualizações sobre a aula de JS

--- Intro JS/Aula 2 - variavéis/script.js
+++ Intro JS/Aula 2 - variavéis/script.js
@@ -7,3 +7,23 @@ console.log(typeof(vOuF));
 // number
 var numeroQualquer = 1;
 console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'elaine';
+console.log(typeof(nome));
+
+// function
+var funcao = function() {}
+console.log(typeof(funcao));
+
+// como declarar
+/*var variavel = 'elaine';
+variavel = 'patricia';
+console.log(variavel); */
+
+/*let variavel2 = 'elaine';
+variavel2 = 'patricia';
+console.log(variavel2); */
+
+const constante = 'elaine';
+console.log(constante);
