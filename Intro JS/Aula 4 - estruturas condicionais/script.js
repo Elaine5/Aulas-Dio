5e89d00bb79dc708b0bc026b18ad08960cf042dd
Elaine5 <lanne9873@gmail.com>
2022-03-23 20:20:11 -0300

    atualizações do assunto de JS

--- Intro JS/Aula 4 - estruturas condicionais/script.js
+++ Intro JS/Aula 4 - estruturas condicionais/script.js
@@ -1,7 +1,12 @@
-var jogador1 = 1;
-var jogador2 = 0;
+var jogador1 = 0;
+var jogador2 = 1;
 var placar;
 
+// if = se / else = senão
 if (jogador1 > 0) {
-    console.log('JOgador 1 marcou ponto');
+    console.log('Jogador 1 marcou ponto');
+} else if (jogador2 > 0) {
+    console.log('Jogador 2 marcou ponto');
+} else {
+    console.log('Ninguém marcou pono');
 }
