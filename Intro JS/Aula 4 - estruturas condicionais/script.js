5f05f0ca2e1d8848ef0d1595bd7c3608ed33bfa1
Elaine5 <lanne9873@gmail.com>
2022-03-25 15:16:47 -0300

    Atualizações sobre o curso de JavaScript

--- Intro JS/Aula 4 - estruturas condicionais/script.js
+++ Intro JS/Aula 4 - estruturas condicionais/script.js
@@ -1,5 +1,5 @@
-var jogador1 = 0;
-var jogador2 = 1;
+var jogador1 = 1;
+var jogador2 = 0;
 var placar;
 
 // if = se / else = senão
@@ -7,6 +7,19 @@ if (jogador1 > 0) {
     console.log('Jogador 1 marcou ponto');
 } else if (jogador2 > 0) {
     console.log('Jogador 2 marcou ponto');
-} else {
+} 
+// usando else (senão)
+else {
     console.log('Ninguém marcou pono');
+}
+
+switch (placar) {
+    case placar = jogador1 > jogador2:
+        console.log('jogador 1 ganhou');
+        break;
+    case placar = jogador2 > jogador1:
+        console.log('Jogador 2 ganhou');
+        break;
+    default:
+        console.log('Ninguém ganhou')
 }
---
+++ Intro JS/Aula 5/script.js
@@ -0,0 +1,14 @@
+// () indica que é um objeto do tipo function
+// {} indica que é um bloco de instrução
+
+/* function funcao() {
+    console.log('Essa mensagem veio de uma função!!')
+}
+
+funcao(); */
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+
+mensagem('Tudo funcionando :)');
